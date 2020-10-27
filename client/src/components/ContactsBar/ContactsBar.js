a36bafbc19581485a4b7ec15f075ae9fb4c55467
Vitaliy Derda <derdavitaliy@gmail.com>
2020-10-28 01:20:20 +0200

    added contactsBar

---
+++ client/src/components/ContactsBar/ContactsBar.js
@@ -0,0 +1,17 @@
+//Core
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ContactsBar = props => {
+    return (
+        <div>
+            ContactsBar
+        </div>
+    )
+}
+
+ContactsBar.propTypes = {
+
+}
+
+export default ContactsBar
